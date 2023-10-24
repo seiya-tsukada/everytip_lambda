c62f7537156c000d28667805a44f9f395e1756e6
Seiya Tsukada <seiya.tsukada@paypay-corp.co.jp>
2023-10-24 15:05:19 +0900

    adjustment

--- lambda_function.py
+++ lambda_function.py
@@ -17,42 +17,43 @@ def lambda_handler(event, context):
     url = "https://slack.com/api/chat.postMessage"
     token = env = os.environ["SLACK_OAUTH_TOKEN"]
    
-    print(token)
+    pprint.pprint(token)
 
     if event:
         body = base64.b64decode(event["body"]).decode("utf-8")
         res = parse_qs(body)
     else:
-        res = "hello fpos"
+        res = "hello everytip"
    
-    text = res["text"]
-    print(res)
+    
+
+    pprint.pprint(res)
+
+
     print(res["user_id"])
     print(res["user_name"])
     print(res["channel_id"])
+    text = res["text"]
     print("text: " + text[0])
    
     res_dict = {
         "user_id": res["user_id"],
         "user_name": res["user_name"],
-        "channel_id": res["channel_id"]
-        #"text": res["text"]
+        "channel_id": res["channel_id"],
+        "text": text[0],
     }
    
-    print(type(text))
+    print("text tye: " + type(text))
     pprint.pprint(res_dict)
    
-
-
     data = {
         "channel" : "fpos",
-        "text" : text[0]
-       
+        "text" : text[0],
     }
 
     method = 'POST'
     # request_headers = { 'Content-Type': 'application/json; charset=utf-8' }
-    headers = { "Authorization": "Bearer {}".format(token), 'Content-Type': 'application/json; charset=utf-8' }
+    headers = { "Authorization": "Bearer {}".format(token), "Content-Type": "application/json; charset=utf-8" }
     body = json.dumps(data).encode("utf-8")
     request = urllib.request.Request(
         url=url,
@@ -63,7 +64,7 @@ def lambda_handler(event, context):
     with urllib.request.urlopen(request) as res:
         ans = res.read()
    
-    print(ans)
+    pprint.pprint(ans)
    
    
    
