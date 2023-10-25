d9805670d09502641e6d79e0d001ba5c48f479f4
Seiya Tsukada <seiya.tsukada@paypay-corp.co.jp>
2023-10-25 16:26:24 +0900

    add dynamo operation

--- lambda_function.py
+++ lambda_function.py
@@ -163,9 +163,9 @@ def dynamo_insert(user_id, user_name, amount):
         "TableName": DDB_TABLE_NAME,
         # "Item": data
         "Item": {
-            "user_id": user_id,
-            "user_name": user_name,
-            "wallet": amount,
+            "user_id": "user_id",
+            "user_name": "user_name",
+            "wallet": "amount",
             "attr1": "attr1111",
             "attr2": "attr2222"
         }
